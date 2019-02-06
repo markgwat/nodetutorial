e8b7d6e5e422a0a29b0bf88e60789fe8313aef37
Mark Watson <mark.g.watson@protonmail.com>
2019-02-06 14:20:57 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+const fs = require('fs');
+
+// Define partials directory
+hbs.registerPartials(__dirname + '/views/partials');
+// Register Partial segments with arguments
+hbs.registerPartial('footer', '{{getCurrentYear}}');
+hbs.registerPartial('header', '{{title}}');
+
+// Define template engine
+app.set('view engine', 'hbs');
+
+// Middleware
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = now;
+    console.log(now);
+    fs.appendFile('server.log',log+' '+req.method+' '+req.url+'\n', (err) => {
+        if(err){
+            console.log('Unable to write file');
+        }
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance.hbs',{
+    });
+//    next();
+});
+
+
+// Define path
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+});
+app.get('/', (req, res) => {
+//  //   res.send('<h1>Hello Express!</h1>');
+//  res.send({
+//      name: 'Mark',
+//      likes: [
+//          'Cars',
+//          'Bikes'
+//      ]
+//  })
+    res.render('home.hbs',{
+        title: 'Home Page',
+        welcome: 'Hi And Welcome'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        title: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad Request'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is on port 3000');
+});
